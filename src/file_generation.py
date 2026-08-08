7c140df52eba25df8e1c4206133de9ec74e559fd
EvanFarris <evanfarris0@gmail.com>
2026-08-08 16:47:08 -0500

    Script builds to docs/ instead of public/ for github site deployment

--- src/file_generation.py
+++ src/file_generation.py
@@ -17,7 +17,7 @@ def extract_title(markdown):
             return line[2:].strip()
     raise ValueError("No h1 found")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath="/"):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     
     with open(from_path, "r") as f:
@@ -32,7 +32,8 @@ def generate_page(from_path, template_path, dest_path):
     
     result = template_content.replace("{{ Title }}", title)
     result = result.replace("{{ Content }}", htmlstr)
-
+    result = result.replace('href="/', 'href="{basepath}')
+    result = result.replace('src="/', 'src="{basepath}')
     pathonly = dest_path.split("/")
     pathonly = pathonly[:-1]
     pathonly = "/".join(pathonly)
@@ -42,13 +43,13 @@ def generate_page(from_path, template_path, dest_path):
     with open(dest_path, "w") as f:
         f.write(result)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath = "/"):
     for obj in os.listdir(dir_path_content):
         obj_dir_path = os.path.join(dir_path_content, obj)
         
         if os.path.isfile(obj_dir_path):
             obj_dest_path = os.path.join(dest_dir_path, obj[:-2] + "html")
-            generate_page(obj_dir_path, template_path, obj_dest_path)
+            generate_page(obj_dir_path, template_path, obj_dest_path, basepath)
         else:
             obj_dest_path = os.path.join(dest_dir_path, obj)
-            generate_pages_recursive(obj_dir_path, template_path, obj_dest_path)
+            generate_pages_recursive(obj_dir_path, template_path, obj_dest_path, basepath)
--- src/main.py
+++ src/main.py
@@ -1,16 +1,17 @@
 from textnode import TextNode, TextType
 from file_generation import copy_static, generate_page, generate_pages_recursive
-
+import sys
 
 def main():
+    if len(sys.argv) == 1:
+        basepath = "/"
+    else:
+        basepath = sys.argv[1]
+        
     testNode = TextNode("This is some anchor text", TextType["LINK"], "https://www.boot.dev")
     copy_static()
-    generate_pages_recursive("content/", "template.html", "public/")
-    #generate_page("content/index.md", "template.html", "public/index.html")
-    #generate_page("content/blog/glorfindel/index.md", "template.html", "public/blog/glorfindel/index.html")
-    #generate_page("content/blog/tom/index.md", "template.html", "public/blog/tom/index.html")
-    #generate_page("content/blog/majesty/index.md", "template.html", "public/blog/majesty/index.html")
-    #generate_page("content/contact/index.md", "template.html", "public/contact/index.html")
+    generate_pages_recursive("content/", "template.html", "docs/", basepath)
+
 if __name__ == "__main__":
     main()
     
