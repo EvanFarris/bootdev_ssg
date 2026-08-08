2402d8c1df1e561f0febc4f7920bfdca514c98e9
EvanFarris <evanfarris0@gmail.com>
2026-08-08 17:09:56 -0500

    Fixed carriage return bug

--- src/main.py
+++ src/main.py
@@ -3,11 +3,12 @@ from file_generation import copy_static, generate_page, generate_pages_recursive
 import sys
 
 def main():
+    basepath = ""
     if len(sys.argv) == 1:
         basepath = "/"
     else:
-        basepath = sys.argv[1] + "/"
-        
+        basepath = sys.argv[1].strip() + "/"
+
     testNode = TextNode("This is some anchor text", TextType["LINK"], "https://www.boot.dev")
     copy_static()
     generate_pages_recursive("content/", "template.html", "docs/", basepath)
